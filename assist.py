4746120c6703e1d3740c7f9f2f4bfbb1179674c1
elvissierra <elvissierra3@gmail.com>
2024-05-29 11:19:18 -0500

    initial

---
+++ assist.py
@@ -0,0 +1,29 @@
+import environ
+import os
+import wandb
+from openai import OpenAI
+client = OpenAI()
+
+env = environ.Env()
+environ.Env.read_env()
+
+api_key = env("OPENAI_API_KEY")
+
+gpt_assistant_prompt = "You're a " + input ("Who am I in this scenario?")
+gpt_user_prompt = input ("What prompt am I to do?")
+gpt_prompt = gpt_assistant_prompt, gpt_user_prompt
+print(gpt_prompt)
+
+message= [{"role": "assistant", "content": gpt_assistant_prompt}, {"role": "user", "content" : gpt_user_prompt}]
+temperature = 0.2
+max_tokens = 256
+frequency_penalty = 0.0
+
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo-0125",
+    messages=message,
+    temperature=temperature,
+    max_tokens=max_tokens,
+    frequency_penalty=frequency_penalty
+)
+print(response.choices[0].message)
